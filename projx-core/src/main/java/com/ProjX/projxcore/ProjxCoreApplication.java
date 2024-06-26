64f44d67663bb00fdbd24c64382e2371b3ae4e8b
LSBogdan <lolotstefanbogdan@gmail.com>
2024-06-26 16:36:29 +0300

    spring server cloud started

--- projx-core/src/main/java/com/ProjX/projxcore/ProjxCoreApplication.java
+++ projx-core/src/main/java/com/ProjX/projxcore/ProjxCoreApplication.java
@@ -5,6 +5,7 @@ import com.ProjX.projxpersitance.mappers.customMapppers.CustomMappersImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.ComponentScan;
 
 import java.lang.reflect.InvocationTargetException;
@@ -12,6 +13,7 @@ import java.lang.reflect.InvocationTargetException;
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.ProjX.projxcore", "com.ProjX.projxpersitance"})
 @EntityScan("com.ProjX.projxpersitance.entitys")
+@EnableConfigServer
 public class ProjxCoreApplication {
 
 	public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
